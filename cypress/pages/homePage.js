91b00d40f0161fdc9038a671535c599af873044a
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2024-02-01 14:49:10 +0100

    Added bug to test for debugging reports

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -3,7 +3,7 @@ export class homePage {
     weblocators = {
         
         searchInput: '[name="search"]',
-        clickSearch: '.btn-default',
+        clickSearch: '.btn-default22',
         product: '.product-thumb img[title="MacBook"]',
         addToCart: 'Add to Cart',
         successMessage: 'div.alert.alert-success.alert-dismissible'
